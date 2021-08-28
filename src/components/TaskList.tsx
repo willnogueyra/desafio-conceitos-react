7e2374d3045cd94522eedb83f942686536caa931
William Nogueira <will.ribeiro@live.com>
2021-08-27 22:11:07 -0300

    desafio concluído

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,36 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if (!newTaskTitle) return; // se não preenchido não enviar
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(task => [...task, newTask])
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    const checkTask = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task)
+
+    setTasks(checkTask)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    const filterTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(filterTasks);
   }
 
   return (
